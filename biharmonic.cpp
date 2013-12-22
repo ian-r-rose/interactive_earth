9df1b8281fa4597afba1068dd814539cd082a3f0
ian-r-rose <ian.r.rose@gmail.com>
2013-12-22 15:48:46 -0800

    Mostly working laplace solve now

--- biharmonic.cpp
+++ biharmonic.cpp
@@ -6,6 +6,7 @@
 #include <Epetra_CrsGraph.h>
 #include <Epetra_Time.h>
 #include <AztecOO.h>
+#include <Amesos.h>
 #include <ml_include.h>
 #include <Epetra_LinearProblem.h>
 #include <ml_MultiLevelOperator.h>
@@ -13,21 +14,24 @@
 
 #include "staggered_grid.h"
 #include <vector>
+#include <cmath>
+#include <fstream>
 
+double heat(const Point &p)
+{
+  return -std::exp( (-(0.5-p.x)*(0.5-p.x) - (0.5-p.y)*(0.5-p.y) )/.05);
+}
 
 int main(int argc, char **argv)
 {
   Epetra_SerialComm Comm;
 
-  int nx = 10, ny = 5;
+  int nx = 100, ny = 100;
   int ncells = nx*ny;
 
   Epetra_Map map(ncells, 0, Comm);
 
-  Epetra_Vector stream(map);
-  Epetra_Vector vorticity(map);
-  Epetra_Vector u(map);
-  Epetra_Vector v(map);
+  Epetra_Vector rhs(map);
   Epetra_Vector T(map);
 
   StaggeredGrid grid(1.0, 1.0, nx, ny);
@@ -42,6 +46,7 @@ int main(int argc, char **argv)
     {
       indices.push_back(cell->self());
       values.push_back(1.0);
+      rhs[cell->self()] = 0.0;     
     }
     else
     {
@@ -50,13 +55,28 @@ int main(int argc, char **argv)
       indices.push_back( cell->right() ); values.push_back(1.0);
       indices.push_back( cell->up() ); values.push_back(1.0);
       indices.push_back( cell->down() ); values.push_back(1.0);
+      rhs[cell->self()] = heat(cell->corner());
     }
 
     int ierr =  mat.InsertGlobalValues(cell->self(), indices.size(), &values[0], &indices[0]);
-    std::cout<<ierr<<std::endl;
   }
   mat.FillComplete();
-
-  
  
+  
+  Epetra_LinearProblem laplace_problem;
+  laplace_problem.SetOperator(&mat);
+  laplace_problem.SetLHS(&T);
+  laplace_problem.SetRHS(&rhs);
+
+  Amesos Amesos_Factory;
+  Amesos_BaseSolver *Solver;
+  Solver = Amesos_Factory.Create("Amesos_Klu", laplace_problem);
+  Solver->SymbolicFactorization(); 
+  Solver->NumericFactorization();
+  Solver->Solve();
+
+  std::ofstream file("out.txt");
+  for( StaggeredGrid::iterator cell = grid.begin(); cell != grid.end(); ++cell)
+    file<<cell->corner().x<<"\t"<<cell->corner().y<<"\t"<<T[cell->self()]<<std::endl;
+  file.close(); 
 } 
