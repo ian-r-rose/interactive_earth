b98473f83efdc23c8b3d567d07271637379d0e05
Ian Rose <ian.r.rose@gmail.com>
2015-03-02 09:08:53 -0800

    Remove useless colormap

--- color.h
+++ color.h
@@ -15,22 +15,3 @@ color hot(double x)
   c.B = (x < green_cutoff ? 0.0 : (x-green_cutoff)/(1.0-green_cutoff));
   return c;
 }
-
-color hot_with_composition( double x, double y)
-{
-  color c;
-  double red_cutoff = 0.4;
-  double green_cutoff = 0.8;
-  c.R = (x < red_cutoff ?  (x/red_cutoff) : 1.0);
-  c.G = (x < red_cutoff ? 0.0 : (x < green_cutoff ? (x-red_cutoff)/(green_cutoff-red_cutoff) : 1.0));
-  c.B = (x < green_cutoff ? 0.0 : (x-green_cutoff)/(1.0-green_cutoff));
-
-  if(y > 0.8)
-  {
-    c.R = 0.2*c.R;
-    c.G = 0.2*c.G;
-    c.B = 0.8;
-  }
-
-  return c;
-}
