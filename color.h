f84544a83a17d68886cef661d04e0a0993c7af24
Ian Rose <ian.r.rose@gmail.com>
2016-03-22 14:24:28 -0700

    Adjust colormap.

--- color.h
+++ color.h
@@ -8,8 +8,8 @@ struct color
 color hot(double x)
 {
   color c;
-  double red_cutoff = 0.4;
-  double green_cutoff = 0.8;
+  double red_cutoff = 0.3;
+  double green_cutoff = 0.7;
   c.R = (x < red_cutoff ?  (x/red_cutoff) : 1.0);
   c.G = (x < red_cutoff ? 0.0 : (x < green_cutoff ? (x-red_cutoff)/(green_cutoff-red_cutoff) : 1.0));
   c.B = (x < green_cutoff ? 0.0 : (x-green_cutoff)/(1.0-green_cutoff));
